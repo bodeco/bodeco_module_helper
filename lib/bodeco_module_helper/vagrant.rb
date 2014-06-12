c735eb42fc1b023eaeba4146efad6a6cb9a64740
Nan Liu <nan.liu@gmail.com>
2014-06-12 09:39:37 -0700

    Add vagrant vm support.

---
+++ lib/bodeco_module_helper/vagrant.rb
@@ -0,0 +1,31 @@
+def vm(opt)
+  module_name = opt.fetch(:module).to_s || raise(ArgumentError, 'Must provide puppet module name')
+  hostname = opt.fetch(:hostname, module_name).to_s
+  memory = opt.fetch(:memory, 512)
+  cpu = opt.fetch(:cpu, 1)
+  box = opt.fetch(:box).to_s || raise(ArgumentError, 'Must provide box type.')
+  url = opt.fetch(:url).to_s
+
+  Vagrant.configure('2') do |conf|
+
+    conf.vm.synced_folder './', "/etc/puppet/modules/#{module_name}"
+
+    conf.vm.define module_name.to_sym do |mod|
+      mod.vm.box = box
+      mod.vm.box_url = url
+
+      mod.vm.hostname = hostname
+      mod.vm.provider :vmware_fusion do |f|
+        f.vmx['displayName'] = hostname
+        f.vmx['memsize'] = memory
+        f.vmx['numvcpus'] = cpu
+      end
+
+      mod.vm.provision :puppet do |p|
+        p.manifests_path = 'tests'
+        p.manifest_file = ENV['VAGRANT_MANIFEST'] || 'init.pp'
+      end
+    end
+  end
+end
+
